d07acf6f6f3d8acd068cc1aa11d20e826d4f1d8d
Kialan Pillay <kialanpillay@gmail.com>
2020-10-19 17:21:07 +0200

    Create Cycling.java

---
+++ Cycling.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Cycling{
+    public static void main(String[]args){
+        Scanner read = new Scanner(System.in);
+        int N = read.nextInt();
+        int[]P = new int[N];
+
+        for(int i = 0; i < N; i++){
+            P[i] = read.nextInt();
+        }
+
+        int[][]SS = new int[N][N];
+
+        int max = 0;
+
+        for(int y = 0; y < N; y++){
+            for(int x = 0; x < N; x++){
+                if(y == 0){
+                    //System.out.print(String.format("%-5s", P[x]));
+                }
+                if(x == 0){
+                    SS[y][x] = 0 + P[(y+x)%N];
+                }
+                else{
+                    SS[y][x] = SS[y][x-1] + P[(y+x)%N];
+                }
+                //System.out.print(String.format("%-5s", SS[y][x]));
+                if(SS[y][x] > max){
+                    max = SS[y][x];
+                }
+            }
+            //System.out.println();
+        }
+        System.out.println(max);
+    }
+}
